22033ba8c630235374c4ccd6f32347d59acb9874
Davide Pedron <davide@happydev.cloud>
2021-12-02 00:24:46 +0100

    Introduce Config object to manage inputs

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,20 @@ use adventofcode2021::run;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let day = &args[1];
-    let part = &args[2];
+    let config = Config::new(&args);
 
-    run(format!("day{}part{}", day, part));
+    run(format!("day{}part{}", config.day, config.part));
+}
+
+struct Config {
+    day: String,
+    part: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let day = args[1].clone();
+        let part = args[2].clone();
+        Config { day, part }
+    }
 }

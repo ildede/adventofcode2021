998551462b518e626b7f3a0b2d776aee803a6a61
Davide Pedron <davide@happydev.cloud>
2021-12-02 00:46:46 +0100

    Move Config to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,40 @@
 use std::fs;
 
+pub struct Config {
+    pub day: u8,
+    pub part: u8,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        if args.len() > 3 {
+            return Err("too many arguments");
+        }
+
+        let day = match args[1].parse() {
+            Ok(n) => {
+                n
+            },
+            Err(_) => {
+                return Err("first argument (day) must be a number")
+            },
+        };
+        let part: u8 = match args[2].parse() {
+            Ok(n) => {
+                n
+            },
+            Err(_) => {
+                return Err("second argument (part) must be a number")
+            },
+        };
+
+        Ok(Config { day, part })
+    }
+}
+
 pub fn run(filename: String) {
     println!("Filename: {}", filename);
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{env, process};
 
-use adventofcode2021::run;
+use adventofcode2021::{Config, run};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,39 +11,4 @@ fn main() {
     });
 
     run(format!("day{:}part{:}", config.day, config.part));
-}
-
-struct Config {
-    day: u8,
-    part: u8,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        if args.len() > 3 {
-            return Err("too many arguments");
-        }
-
-        let day = match args[1].parse() {
-            Ok(n) => {
-                n
-            },
-            Err(_) => {
-                return Err("first argument (day) must be a number")
-            },
-        };
-        let part: u8 = match args[2].parse() {
-            Ok(n) => {
-                n
-            },
-            Err(_) => {
-                return Err("second argument (part) must be a number")
-            },
-        };
-
-        Ok(Config { day, part })
-    }
 }

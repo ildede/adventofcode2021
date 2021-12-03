da80c4618c28105aed574ad2cc4eaf9cde41e00e
Davide Pedron <davide@happydev.cloud>
2021-12-03 18:29:29 +0100

    Solve day 3 part 1

--- src/day3.rs
+++ src/day3.rs
@@ -9,10 +9,10 @@ pub fn solve_puzzle(part: u8, contents: String) -> String {
     }
 }
 
-fn convert_to_vec(contents: String) -> Vec<(i32, i32)> {
-    let mut vec = vec![(0, 0), (0, 0), (0, 0), (0, 0), (0, 0)];
+fn convert_to_vec(contents: String) -> Vec<(u32, u32)> {
+    let mut vec: Vec<(u32, u32)> = vec![(0, 0); contents.lines().nth(0).unwrap().len()];
     for line in contents.lines() {
-        for n in 0..5 {
+        for n in 0..line.len() {
             if line.chars().nth(n).unwrap() == '0' {
                 vec[n].0 = vec[n].0 + 1;
             } else {
@@ -23,44 +23,30 @@ fn convert_to_vec(contents: String) -> Vec<(i32, i32)> {
     vec
 }
 
-fn get_gamma_rate(vec: &Vec<(i32, i32)>) -> u8 {
-    let mut conversion: u8 = 0;
-    if vec[4].1 > vec[4].0 {
-        conversion = conversion + 1;
+fn get_gamma_rate(vec: &Vec<(u32, u32)>) -> u32 {
+    let mut str  = String::new();
+    for n in 0..vec.len() {
+        if vec[n].0 < vec[n].1 {
+            str.push('1');
+        } else {
+            str.push('0');
+        }
     }
-    if vec[3].1 > vec[3].0 {
-        conversion = conversion + 2;
-    }
-    if vec[2].1 > vec[2].0 {
-        conversion = conversion + 4;
-    }
-    if vec[1].1 > vec[1].0 {
-        conversion = conversion + 8;
-    }
-    if vec[0].1 > vec[0].0 {
-        conversion = conversion + 16;
-    }
-    conversion
+    let intval = isize::from_str_radix(&*str, 2).unwrap();
+    intval as u32
 }
 
-fn get_epsilon_rate(vec: &Vec<(i32, i32)>) -> u8 {
-    let mut conversion: u8 = 0;
-    if vec[4].1 < vec[4].0 {
-        conversion = conversion + 1;
+fn get_epsilon_rate(vec: &Vec<(u32, u32)>) -> u32 {
+    let mut str  = String::new();
+    for n in 0..vec.len() {
+        if vec[n].0 > vec[n].1 {
+            str.push('1');
+        } else {
+            str.push('0');
+        }
     }
-    if vec[3].1 < vec[3].0 {
-        conversion = conversion + 2;
-    }
-    if vec[2].1 < vec[2].0 {
-        conversion = conversion + 4;
-    }
-    if vec[1].1 < vec[1].0 {
-        conversion = conversion + 8;
-    }
-    if vec[0].1 < vec[0].0 {
-        conversion = conversion + 16;
-    }
-    conversion
+    let intval = isize::from_str_radix(&*str, 2).unwrap();
+    intval as u32
 }
 
 #[cfg(test)]
@@ -84,44 +70,58 @@ mod tests {
 
     #[test]
     fn get_min_gamma_rate() {
-        let input: Vec<(i32, i32)> = vec![(1, 0), (1, 0), (1, 0), (1, 0), (1, 0)];
+        let input: Vec<(u32, u32)> = vec![(1, 0), (1, 0), (1, 0), (1, 0), (1, 0)];
 
         assert_eq!(0, get_gamma_rate(&input));
     }
 
     #[test]
     fn get_min_epsilon_rate() {
-        let input: Vec<(i32, i32)> = vec![(0, 1), (0, 1), (0, 1), (0, 1), (0, 1)];
+        let input: Vec<(u32, u32)> = vec![(0, 1), (0, 1), (0, 1), (0, 1), (0, 1)];
 
         assert_eq!(0, get_epsilon_rate(&input));
     }
 
     #[test]
     fn get_max_gamma_rate() {
-        let input: Vec<(i32, i32)> = vec![(0, 1), (0, 1), (0, 1), (0, 1), (0, 1)];
+        let input: Vec<(u32, u32)> = vec![(0, 1), (0, 1), (0, 1), (0, 1), (0, 1)];
 
         assert_eq!(31, get_gamma_rate(&input));
     }
 
     #[test]
     fn get_max_epsilon_rate() {
-        let input: Vec<(i32, i32)> = vec![(1, 0), (1, 0), (1, 0), (1, 0), (1, 0)];
+        let input: Vec<(u32, u32)> = vec![(1, 0), (1, 0), (1, 0), (1, 0), (1, 0)];
 
         assert_eq!(31, get_epsilon_rate(&input));
     }
 
     #[test]
     fn get_gamma_rate_example_part_one() {
-        let input: Vec<(i32, i32)> = vec![(5, 7), (7, 5), (4, 8), (5, 7), (7, 5)];
+        let input: Vec<(u32, u32)> = vec![(5, 7), (7, 5), (4, 8), (5, 7), (7, 5)];
 
         assert_eq!(22, get_gamma_rate(&input));
     }
 
     #[test]
     fn get_epsilon_rate_example_part_one() {
-        let input: Vec<(i32, i32)> = vec![(5, 7), (7, 5), (4, 8), (5, 7), (7, 5)];
+        let input: Vec<(u32, u32)> = vec![(5, 7), (7, 5), (4, 8), (5, 7), (7, 5)];
 
         assert_eq!(9, get_epsilon_rate(&input));
+    }
+
+    #[test]
+    fn get_max_gamma_rate_of_long_vec() {
+        let input: Vec<(u32, u32)> = vec![(0, 1), (0, 1), (0, 1), (0, 1), (0, 1), (0, 1), (0, 1), (0, 1), (0, 1), (0, 1), (0, 1), (0, 1)];
+
+        assert_eq!(4095, get_gamma_rate(&input));
+    }
+
+    #[test]
+    fn get_max_epsilon_rate_of_long_vec() {
+        let input: Vec<(u32, u32)> = vec![(1, 0), (1, 0), (1, 0), (1, 0), (1, 0), (1, 0), (1, 0), (1, 0), (1, 0), (1, 0), (1, 0), (1, 0)];
+
+        assert_eq!(4095, get_epsilon_rate(&input));
     }
 
     #[test]
@@ -129,5 +129,15 @@ mod tests {
         let contents = String::from("00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010");
 
         assert_eq!(vec![(5, 7), (7, 5), (4, 8), (5, 7), (7, 5)], convert_to_vec(contents));
+    }
+
+    #[test]
+    fn conversion_from_string_to_bitsum_vec_with_arbitrary_length_of_binary_number() {
+        let contents = String::from("110001010110");
+
+        assert_eq!(
+            vec![(0, 1), (0, 1), (1, 0), (1, 0), (1, 0), (0, 1), (1, 0), (0, 1), (1, 0), (0, 1), (0, 1), (1, 0)],
+            convert_to_vec(contents)
+        );
     }
 }

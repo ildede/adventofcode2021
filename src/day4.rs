eb9056243ccc36fc865dd08e7c22795f5cf73ac1
Davide Pedron <davide@happydev.cloud>
2021-12-06 03:23:16 +0100

    Add simple conversion

--- src/day4.rs
+++ src/day4.rs
@@ -2,19 +2,40 @@ use crate::utils::convert_to_vec;
 
 pub fn solve_puzzle(part: u8, contents: String) -> String {
     let rows_of_content = convert_to_vec(contents);
-    for x in rows_of_content {
-        println!("{}", x);
-    }
     match part {
-        1 => unimplemented!("not implemented yet"),
+        1 => {
+            let drawn_numbers = to_drawn_numbers(&rows_of_content[0]);
+            String::from("")
+        },
         2 => unimplemented!("not implemented yet"),
         _ => panic!("invalid part")
     }
 }
 
+fn to_drawn_numbers(list: &String) -> Vec<u8> {
+    let split = list.split(',')
+        .map(|c| c.parse::<u8>().unwrap())
+        .collect();
+
+    println!("{:?}", split);
+    split
+}
+
+fn to_bingo_board(input_board: Vec<String>) -> Vec<Vec<u8>> {
+    let mut board: Vec<Vec<u8>> = Vec::new();
+    for n in 0..5 {
+        let mut row: Vec<u8> = Vec::new();
+        for number in input_board[n].split_whitespace() {
+            row.push(number.parse::<u8>().unwrap())
+        }
+        board.push(row);
+    }
+    board
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::day4::solve_puzzle;
+    use crate::day4::{solve_puzzle, to_bingo_board, to_drawn_numbers};
 
     #[test]
     fn test_puzzle_example_part_one() {
@@ -42,4 +63,35 @@ mod tests {
         assert_eq!("4512", solve_puzzle(1, contents));
     }
 
+    #[test]
+    fn converts_to_list_of_drawn_numbers() {
+        let contents = String::from("7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1");
+
+        assert_eq!(
+            vec![7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1],
+            to_drawn_numbers(&contents)
+        )
+    }
+
+    #[test]
+    fn converts_to_bingo_board() {
+        let contents = vec![
+            String::from("22 13 17 11  0"),
+            String::from(" 8  2 23  4 24"),
+            String::from("21  9 14 16  7"),
+            String::from(" 6 10  3 18  5"),
+            String::from(" 1 12 20 15 19")];
+
+        let expected: Vec<Vec<u8>> = vec![
+            vec![22, 13, 17, 11, 0],
+            vec![8, 2, 23, 4, 24],
+            vec![21, 9, 14, 16, 7],
+            vec![6, 10, 3, 18, 5],
+            vec![1, 12, 20, 15, 19]
+        ];
+        assert_eq!(
+            expected,
+            to_bingo_board(contents)
+        )
+    }
 }

8a943e85cbf075ea16189f2d599474db02b20bf8
Davide Pedron <davide@happydev.cloud>
2021-12-09 00:14:45 +0100

    Add runner for all days

---
+++ src/day20.rs
@@ -0,0 +1,10 @@
+use crate::utils::convert_to_vec;
+
+pub fn solve_puzzle(part: u8, contents: String) -> String {
+    let rows_of_content = convert_to_vec(contents);
+    match part {
+        1 => unimplemented!("not implemented yet"),
+        2 => unimplemented!("not implemented yet"),
+        _ => panic!("invalid part")
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,27 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
+mod day6;
+mod day7;
+mod day8;
+mod day9;
+mod day10;
+mod day11;
+mod day12;
+mod day13;
+mod day14;
+mod day15;
+mod day16;
+mod day17;
+mod day18;
+mod day19;
+mod day20;
+mod day21;
+mod day22;
+mod day23;
+mod day24;
+mod day25;
 mod utils;
 
 pub struct Config {
@@ -52,6 +73,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
         2 => day2::solve_puzzle(config.part, contents),
         3 => day3::solve_puzzle(config.part, contents),
         4 => day4::solve_puzzle(config.part, contents),
+        5 => day5::solve_puzzle(config.part, contents),
         _ => panic!("invalid day")
     };
     println!("Result: {}", result);

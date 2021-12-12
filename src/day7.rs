4019ee65d493515e4028b7d24a213980bb582058
Davide Pedron <davide@happydev.cloud>
2021-12-12 19:08:01 +0100

    Solve day 7 part 2

--- src/day7.rs
+++ src/day7.rs
@@ -13,10 +13,26 @@ pub fn solve_puzzle(part: u8, contents: String) -> String {
                 }).sum();
             }
             String::from(results.iter().min().unwrap().to_string())
-        }
-        2 => unimplemented!("not implemented yet"),
+        },
+        2 => {
+            let numbers = split_to_usize(rows_of_content[0].clone());
+            let max = numbers.clone().iter().max().unwrap().clone();
+            let mut results: Vec<usize> = vec![0; max as usize];
+            for i in 0..results.len() {
+                results[i] = numbers.clone()
+                    .iter()
+                    .map(|e| abs_diff(*e, i))
+                    .map(|e | fuel_cost(e))
+                    .sum();
+            }
+            String::from(results.iter().min().unwrap().to_string())
+        },
         _ => panic!("invalid part")
     }
+}
+
+fn fuel_cost(num: usize) -> usize {
+    (1..=num).into_iter().sum()
 }
 
 fn abs_diff(slf: usize, other: usize) -> usize {
@@ -36,5 +52,12 @@ mod tests {
         let contents = String::from("16,1,2,0,4,2,7,1,2,14");
 
         assert_eq!("37", solve_puzzle(1, contents));
+    }
+
+    #[test]
+    fn test_puzzle_example_part_two() {
+        let contents = String::from("16,1,2,0,4,2,7,1,2,14");
+
+        assert_eq!("168", solve_puzzle(2, contents));
     }
 }
